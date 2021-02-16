7a25e6b9ec7c87512293718a2161b19e85b57501
Saketh Mynampati <smynampati42@gmail.com>
2021-02-16 18:03:10 -0500

    Deleted Whitespace

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -43,8 +43,6 @@ public class Robot extends TimedRobot {
     pixy = new PixyCam();
     navX = new AHRS(SPI.Port.kMXP);
     
-    
-    
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
